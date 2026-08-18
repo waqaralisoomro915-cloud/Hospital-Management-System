ade75bebaf60d89e27ab06df467afcb6460be903
Waqar Ali <waqaralisoomro915@gmail.com>
2026-08-18 13:31:55 +0500

    solved validation error in Hospitalization

--- apps/hospitalization/models.py
+++ apps/hospitalization/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-from django.core.exceptions import ValidationError
-
 from ..patients.models import Patient
 from ..doctors.models import Doctor
 from ..departments.models import Department
